9400bd6935bf0d6d84dc8cb0207c3229b66c5eb5
beata <beata@emc2wave.com>
2020-01-22 15:22:23 -0800

    select #player add eventLis to window keyup

--- app.js
+++ app.js
@@ -1,11 +1,17 @@
 function isTouching(a, b) {
-	const aRect = a.getBoundingClientRect();
-	const bRect = b.getBoundingClientRect();
+  const aRect = a.getBoundingClientRect();
+  const bRect = b.getBoundingClientRect();
 
-	return !(
-		aRect.top + aRect.height < bRect.top ||
-		aRect.top > bRect.top + bRect.height ||
-		aRect.left + aRect.width < bRect.left ||
-		aRect.left > bRect.left + bRect.width
-	);
+  return !(
+    aRect.top + aRect.height < bRect.top ||
+    aRect.top > bRect.top + bRect.height ||
+    aRect.left + aRect.width < bRect.left ||
+    aRect.left > bRect.left + bRect.width
+  );
 }
+
+const player = document.querySelector("#player");
+
+window.addEventListener("keyup", function(e) {
+  console.log(e);
+});
