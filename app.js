e2b8caf84d65cf003c9b1321148a2425a3f53937
beata <beata@emc2wave.com>
2020-01-22 16:07:07 -0800

    add else if for right left move change left 100px

--- app.js
+++ app.js
@@ -19,6 +19,12 @@ window.addEventListener("keyup", function(e) {
   } else if (e.key === "ArrowUp" || e.key === "Up") {
     const currentTop = extractPos(player.style.top);
     player.style.top = `${currentTop - 50}px`;
+  } else if (e.key === "RightArrow" || e.key === "Right") {
+    const currentLeft = extractPos(player.style.left);
+    player.style.left = `${currentLeft + 50}px`;
+  } else if (e.key === "LeftArrow" || e.key === "Left") {
+    const currentLeft = extractPos(player.style.left);
+    player.style.left = `${currentLeft - 50}px`;
   }
 });
 
